6373108162ac1a025f70f412add72697db96720d
Tobias Scholze <tobias.scholze+git@me.com>
2023-01-16 19:58:14 +0100

    Added shared ui setup

---
+++ shared/src/androidMain/kotlin/io/github/tscholze/mpcsample/MCPSampleApplication.kt
@@ -0,0 +1,11 @@
+package io.github.tscholze.mpcsample
+
+import android.app.Application
+
+class ShoppeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        TODO()
+    }
+}
