d7abcb2162c2d40a9759563f55c011b5d5c0d59c
Tobias Scholze <tobias.scholze+git@me.com>
2023-01-21 20:32:29 +0100

    Resetted sample to start with more simple approach

---
+++ app/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CMP_Sample"
+include(":androidApp")
+include(":shared")
---
+++ app/shared/src/iosMain/kotlin/io/github/tscholze/cmpsample/Platform.kt
@@ -0,0 +1,9 @@
+package io.github.tscholze.cmpsample
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
