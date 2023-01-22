76b1a9e6347229d0e839081fd08b385b2c9da902
Tobias Scholze <tobias.scholze+git@me.com>
2023-01-22 20:28:38 +0100

    Fixed missing internal operators

--- app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/composables/components/CMPScaffold.kt
+++ app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/composables/components/CMPScaffold.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.mutableStateOf
