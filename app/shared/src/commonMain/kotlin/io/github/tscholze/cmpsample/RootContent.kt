b59c2615627210d8a68919ba70157e1d0a24aa52
Tobias Scholze <tobias.scholze+git@me.com>
2023-01-22 20:17:22 +0100

    Fixed missing internal operators

--- app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/RootContent.kt
+++ app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/RootContent.kt
@@ -2,11 +2,10 @@ package io.github.tscholze.cmpsample
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import io.github.tscholze.cmpsample.composables.layouts.PageLayout
 
 @Composable
-internal fun RootContent(modifier: Modifier = Modifier) {
+internal fun RootContent() {
     PageLayout {
         Text("Root")
         Text("Layout")
