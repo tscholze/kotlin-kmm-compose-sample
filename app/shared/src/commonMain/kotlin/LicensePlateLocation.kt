718524f3e5eef7dc794763a0979732fadb5f798b
Tobias Scholze <tobias.scholze+git@me.com>
2023-01-23 15:51:15 +0100

    Added comments
    re #2

---
+++ app/shared/src/commonMain/kotlin/LicensePlateLocation.kt
@@ -0,0 +1,14 @@
+package io.github.tscholze.cmpsample.models
+
+/**
+ * LicensePlateLocation model
+ *
+ * @property id ID of a license plate like "A"
+ * @property city City of given ID like "Augsburg"
+ * @property state State of given ID like "Bayern"
+ */
+data class LicensePlateLocation(
+    val id: String,
+    val city: String,
+    val state: String
+)
--- app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/RootContent.kt
+++ app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/RootContent.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import io.github.tscholze.cmpsample.composables.layouts.PageLayout
+import io.github.tscholze.cmpsample.models.LicensePlateLocation
 import io.github.tscholze.cmpsample.utils.ResourceReader
 
 @Composable
@@ -33,27 +34,30 @@ internal fun RootContent() {
 }
 
 private fun parseData(): List<LicensePlateLocation> {
+    // 1. Prepare raw data from CSV file.
     val rawData = ResourceReader()
         .readResource("data.csv")
         .split("\r\n")
         .drop(1)
         .map { it.split(",") }
 
+    // 2. Parse given raw data into LicensePlateLocation objects.
     val plates = mutableListOf<LicensePlateLocation>()
     for (column in rawData) {
+        // 2.1 Try to parse raw data
         try {
+            // 2.1.1 Validate input.
             if(column.count() != 3) continue
+
+            // 2.1.2 Add valid parsed locations to list
             plates.add(LicensePlateLocation(column[0], column[1], column[2]))
         } catch (e: Exception) {
+            // 2.2 Catch and log all thrown exception
             println("Ignoring raw data column: $column")
         }
     }
 
+    // Return found LicensePlateLocation
     return plates
 }
 
-data class LicensePlateLocation(
-    val id: String,
-    val city: String,
-    val state: String
-)
