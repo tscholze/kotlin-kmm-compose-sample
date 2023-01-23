377e4f9a54e1d2998eda320ea88eb8373d33528c
Tobias Scholze <tobias.scholze+git@me.com>
2023-01-23 15:45:23 +0100

    Added usage of parsed local data

--- app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/RootContent.kt
+++ app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/RootContent.kt
@@ -1,18 +1,59 @@
 package io.github.tscholze.cmpsample
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import io.github.tscholze.cmpsample.composables.layouts.PageLayout
 import io.github.tscholze.cmpsample.utils.ResourceReader
 
 @Composable
 internal fun RootContent() {
     PageLayout {
-        Text("Root")
-        Text("Layout")
+        LazyColumn {
+            items(parseData()) { row ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    // ID like "A"
+                    Text(row.id, style = MaterialTheme.typography.h3)
+
+                    // City "Augsburg"
+                    // State "Bayern"
+                    Column(horizontalAlignment = Alignment.End, modifier = Modifier.fillMaxSize()) {
+                        Text(row.city, modifier = Modifier.wrapContentWidth(Alignment.End))
+                        Text(row.state, modifier = Modifier.wrapContentWidth(Alignment.End))
+                    }
+                }
+            }
+        }
     }
 }
 
-private fun parseData() {
-    val csvString = ResourceReader().readResource("data.csv")
+private fun parseData(): List<LicensePlateLocation> {
+    val rawData = ResourceReader()
+        .readResource("data.csv")
+        .split("\r\n")
+        .drop(1)
+        .map { it.split(",") }
+
+    val plates = mutableListOf<LicensePlateLocation>()
+    for (column in rawData) {
+        try {
+            if(column.count() != 3) continue
+            plates.add(LicensePlateLocation(column[0], column[1], column[2]))
+        } catch (e: Exception) {
+            println("Ignoring raw data column: $column")
+        }
+    }
+
+    return plates
 }
+
+data class LicensePlateLocation(
+    val id: String,
+    val city: String,
+    val state: String
+)
