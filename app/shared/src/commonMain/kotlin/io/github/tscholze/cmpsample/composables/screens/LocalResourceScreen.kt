ac2f418f34157f3fb7c9e51ab30be715c4ee915e
Tobias Scholze <tobias.scholze+git@me.com>
2023-01-27 15:15:52 +0100

    Added info banner to screen
    re #7

--- app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/composables/screens/LocalResourceScreen.kt
+++ app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/composables/screens/LocalResourceScreen.kt
@@ -1,12 +1,9 @@
 package io.github.tscholze.cmpsample.composables.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +11,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.copperleaf.ballast.navigation.routing.RouterContract
+import androidx.compose.ui.unit.dp
 import com.copperleaf.ballast.navigation.vm.Router
 import io.github.tscholze.cmpsample.composables.components.SearchView
 import io.github.tscholze.cmpsample.composables.layouts.PageLayout
@@ -33,7 +30,9 @@ internal fun LocalResourceScreen(router: Router<AppScreens>) {
     // MARK: - Inner helper -
 
     fun filterPlates(query: String): List<LicensePlateLocation> {
-        if (query.isEmpty()) { return allValues }
+        if (query.isEmpty()) {
+            return allValues
+        }
 
         return allValues.filter {
             it.id.startsWith(query, ignoreCase = true)
@@ -44,26 +43,44 @@ internal fun LocalResourceScreen(router: Router<AppScreens>) {
 
     // MARK: - UI -
 
-    return PageLayout("Local Resources", router) {
+    PageLayout("Local Resources", router) {
         Column {
-            // 1. Search container
-            SearchView(textState)
+            // 1. Info block
+            Column {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp)
+                        .background(MaterialTheme.colors.secondary),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        "The following posts are fetch from an embedded resource",
+                        color = MaterialTheme.colors.onSecondary,
+                        modifier = Modifier.padding(horizontal = 8.dp)
 
-            // 2. List of filtered license plate locations
-            LazyColumn {
-                items(filterPlates(textState.value)) { row ->
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        // ID like "A"
-                        Text(row.id, style = MaterialTheme.typography.h3)
+                    )
+                }
 
-                        // City "Augsburg"
-                        // State "Bayern"
-                        Column(
-                            horizontalAlignment = Alignment.End,
-                            modifier = Modifier.fillMaxSize()
-                        ) {
-                            Text(row.city, modifier = Modifier.wrapContentWidth(Alignment.End))
-                            Text(row.state, modifier = Modifier.wrapContentWidth(Alignment.End))
+                // 2. Search container
+                SearchView(textState)
+
+                // 3. List of filtered license plate locations
+                LazyColumn {
+                    items(filterPlates(textState.value)) { row ->
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            // ID like "A"
+                            Text(row.id, style = MaterialTheme.typography.h3)
+
+                            // City "Augsburg"
+                            // State "Bayern"
+                            Column(
+                                horizontalAlignment = Alignment.End,
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                Text(row.city, modifier = Modifier.wrapContentWidth(Alignment.End))
+                                Text(row.state, modifier = Modifier.wrapContentWidth(Alignment.End))
+                            }
                         }
                     }
                 }
--- app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/composables/screens/RemoteResourceScreen.kt
+++ app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/composables/screens/RemoteResourceScreen.kt
@@ -2,21 +2,16 @@ package io.github.tscholze.cmpsample.composables.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Colors
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -30,7 +25,6 @@ import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
@@ -72,31 +66,49 @@ internal fun RemoteResourceScreen(router: Router<AppScreens>) {
 
     // MARK: - UI -
 
-    return PageLayout("Remote Resources", router) {
-        // Create list of all items
-        LazyColumn {
-            itemsIndexed(posts) { index, post ->
-                Column {
-                    Column(
-                        modifier = Modifier.padding(vertical = 8.dp)
-                            .clickable {
-                             uriHandler.openUri(post.url)
-                            }
-                    ) {
-                        Text(post.title, fontWeight = FontWeight.Medium)
-                        Text(
-                            post.created,
-                            style = MaterialTheme.typography.caption,
-                            fontStyle = FontStyle.Italic
-                        )
-                    }
+    PageLayout("Remote Resources", router) {
+        // 1. Info block
+        Column {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(40.dp)
+                    .background(MaterialTheme.colors.secondary),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    "The following posts are fetch from an url",
+                    color = MaterialTheme.colors.onSecondary,
+                    modifier = Modifier.padding(horizontal = 8.dp)
 
-                    // Show divider if current item is not the last one.
-                    if (index < posts.lastIndex) {
-                        Divider(
-                            color = MaterialTheme.colors.secondaryVariant,
-                            thickness = 0.5.dp
-                        )
+                    )
+            }
+
+            // 2. List of all items
+            LazyColumn {
+                itemsIndexed(posts) { index, post ->
+                    Column {
+                        Column(
+                            modifier = Modifier.padding(vertical = 8.dp)
+                                .clickable {
+                                    uriHandler.openUri(post.url)
+                                }
+                        ) {
+                            Text(post.title, fontWeight = FontWeight.Medium)
+                            Text(
+                                post.created,
+                                style = MaterialTheme.typography.caption,
+                                fontStyle = FontStyle.Italic
+                            )
+                        }
+
+                        // Show divider if current item is not the last one.
+                        if (index < posts.lastIndex) {
+                            Divider(
+                                color = MaterialTheme.colors.secondaryVariant,
+                                thickness = 0.5.dp
+                            )
+                        }
                     }
                 }
             }
