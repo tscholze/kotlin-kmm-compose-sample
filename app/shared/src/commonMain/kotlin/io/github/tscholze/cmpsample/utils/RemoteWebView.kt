99f022408e22abd1e02f3cada95f8fafa8487af6
Tobias Scholze <tobias.scholze+git@me.com>
2023-02-21 17:18:55 +0100

    Added webview implementation for Android
    re #22

--- app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/utils/RemoteWebView.kt
+++ app/shared/src/commonMain/kotlin/io/github/tscholze/cmpsample/utils/RemoteWebView.kt
@@ -3,5 +3,15 @@ package io.github.tscholze.cmpsample.utils
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+/**
+ * Multi platform web view composable that renders web site
+ * for given url
+ *
+ * @param urlString Url to render
+ * @param modifier Modifier that shall be applied
+ *
+ * iOS: iOSMain/utils/
+ * Android: androidMain/utils/
+ */
 @Composable
 internal expect fun RemoteWebView(urlString: String, modifier: Modifier)
